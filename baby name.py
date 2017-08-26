7d69c0603c1a185d5b103aa7738a192e2f97c8f9
Kawin <kawinstark@gmail.com>
2017-08-26 18:41:09 +0530

    Update baby name.py

--- baby name.py
+++ baby name.py
@@ -5,18 +5,23 @@ vowels, consonants, letters='aeiou','bcdfghjklmnpqrstvwxyz', vowels+consonants
 terms = int(input("How many letters you want for your baby's name? "))
 
 def babyname():
-    j = []
-    for i in range(1, terms+1):
+    choices = []
+    for x in range(terms):
         k = input("Would you like a [v]owel or [c]onsonant or any [l]etter? ")
-        if k.lower() == 'v':
-            j.append(random.choice(vowels))
-        elif a[k] == 'c':
-            j.append(random.choice(consonants))
-        elif a[k] == 'l':
-            j.append(random.choice(letters))
-        else:
-            print("Unkonown input " + k)
-    print(''.join(j))
+        choices.append(k)
+        
+    for x in range(10):
+        j = []
+        for k in choices:
+            if k.lower() == 'v':
+                j.append(random.choice(vowels))
+            elif k.lower() == 'c':
+                j.append(random.choice(consonants))
+            elif k.lower() == 'l':
+                j.append(random.choice(letters))
+            else:
+                print("Unkonown input " + k)
+        print(''.join(j))
     
 babyname()
 
